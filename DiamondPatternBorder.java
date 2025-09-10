994c34e3eea6f955974b99a5b1bf66ee3e0cda81
Nampally <nampallyharish4@users.noreply.github.com>
2025-09-10 13:39:25 +0530

    Add launch configurations for DiamondPatternBorder and DaimondPatternBorder in launch.json

---
+++ DiamondPatternBorder.java
@@ -0,0 +1,41 @@
+public class DiamondPatternBorder {
+    public static void main(String[] args) {
+        int n = 7; 
+        
+        
+        for (int i = 0; i < n / 2 + 1; i++) {
+          
+            for (int j = 0; j < n / 2 - i; j++) {
+                System.out.print("  ");
+            }
+            
+            System.out.print("* ");
+            
+            for (int j = 0; j < 2 * i - 1; j++) {
+                System.out.print("  ");
+            }
+            if (i > 0) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        
+        
+        for (int i = n / 2 - 1; i >= 0; i--) {
+           
+            for (int j = 0; j < n / 2 - i; j++) {
+                System.out.print("  ");
+            }
+            
+            System.out.print("* ");
+            
+            for (int j = 0; j < 2 * i - 1; j++) {
+                System.out.print("  ");
+            }
+            if (i > 0) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
